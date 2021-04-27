5e561918fdde47e74c2f920ca8e8d2f4e32db18a
Nearsyh <nearsyh@gmail.com>
2021-04-27 18:22:49 +0800

    Add parameters for list items

--- src/routes/reader/stream.rs
+++ src/routes/reader/stream.rs
@@ -1,26 +1,53 @@
+use crate::middlewares::auth::AuthUser;
+use crate::middlewares::di::Services;
 use rocket_contrib::json::Json;
 use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Item {
-    pub id: i64,
-    pub direct_stream_ids: Vec<String>,
-    pub timestamp_usec: i64,
+  pub id: i64,
+  pub direct_stream_ids: Vec<String>,
+  pub timestamp_usec: i64,
 }
 
-#[get("/api/0/stream/items/ids")]
-pub async fn get_item_ids() -> Json<Vec<Item>> {
-    Json(vec![])
+enum FilterType {
+  UNREAD,
+  STARRED,
+}
+
+impl FilterType {
+  fn from_params(s: &str, xt: &str) -> FilterType {
+    if s.ends_with("/state/com.google/starred") {
+      FilterType::STARRED
+    } else {
+      FilterType::UNREAD
+    }
+  }
+}
+
+#[get("/api/0/stream/items/ids?<s>&<xt>&<n>&<r>")]
+pub async fn get_item_ids(
+  auth_user: AuthUser,
+  services: &Services,
+  s: &str,
+  xt: &str,
+  n: usize,
+  r: &str,
+) -> Json<Vec<Item>> {
+  match FilterType::from_params(s, xt) {
+    FilterType::STARRED => Json(vec![]),
+    FilterType::UNREAD => Json(vec![]),
+  }
 }
 
 #[derive(Serialize)]
 pub struct Contents {
-    direction: String,
-    id: String,
-    title: String,
-    description: String,
-    updated: u64,
-    items: Vec<Content>,
+  direction: String,
+  id: String,
+  title: String,
+  description: String,
+  updated: u64,
+  items: Vec<Content>,
 }
 
 #[derive(Serialize)]
@@ -28,12 +55,12 @@ pub struct Content {}
 
 #[get("/api/0/stream/contents")]
 pub async fn get_contents() -> Json<Contents> {
-    Json(Contents {
-        direction: "ltr".to_string(),
-        id: "id".to_string(),
-        title: "Reading List".to_string(),
-        description: "Reading List".to_string(),
-        updated: 1619362681,
-        items: vec![],
-    })
+  Json(Contents {
+    direction: "ltr".to_string(),
+    id: "id".to_string(),
+    title: "Reading List".to_string(),
+    description: "Reading List".to_string(),
+    updated: 1619362681,
+    items: vec![],
+  })
 }
