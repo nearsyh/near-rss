947cd77973387c12e74fd192637c6f818d9e86f7
Nearsyh <nearsyh@gmail.com>
2021-04-27 17:43:58 +0800

    Register the method to handle adding subscription

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ async fn rocket() -> _ {
             routes![
                 routes::reader::ping,
                 routes::reader::subscriptions::list_subscriptions,
+                routes::reader::subscriptions::add_subscription,
                 routes::reader::users::get_user_info,
                 routes::reader::stream::get_item_ids,
                 routes::reader::stream::get_contents,
