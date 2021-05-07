2fc5f29f32cd05158237107b817f9050721ff7d1
Near <nearsy.h@gmail.com>
2021-05-07 21:15:30 +0800

    Disable sqlx logs

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,8 +2,12 @@ pub mod items;
 pub mod subscriptions;
 pub mod users;
 
-use sqlx::SqlitePool;
+use log::LevelFilter;
+use sqlx::{SqlitePool, ConnectOptions, sqlite::SqliteConnectOptions};
+use std::str::FromStr;
 
 pub async fn in_memory_pool() -> SqlitePool {
-    SqlitePool::connect("sqlite::memory:").await.unwrap()
+    let mut option = SqliteConnectOptions::from_str("sqlite::memory:").unwrap();
+    option.log_statements(LevelFilter::Off);
+    SqlitePool::connect_with(option).await.unwrap()
 }
