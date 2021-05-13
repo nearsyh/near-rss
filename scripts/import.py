5c7a2fe43b9eca684b01ec80aada5ae232145bcb
Near <nearsy.h@gmail.com>
2021-05-13 21:26:57 +0800

    Add authorization

--- scripts/import.py
+++ scripts/import.py
@@ -5,6 +5,12 @@ import requests
 if __name__ == "__main__":
     filename = sys.argv[1]
     server = sys.argv[2]
+    username = sys.argv[3]
+    password = sys.argv[4]
+
+    response = requests.post(server + "/accounts/ClientLogin", data={"Email": username, "Passwd": password})
+    token = response.text.splitlines()[-1][5:]
+    headers = {"Authorization": "GoogleLogin auth=" + token}
 
     root = ET.parse(filename).getroot()
     for body in root.findall("body"):
@@ -12,5 +18,5 @@ if __name__ == "__main__":
             title = folder.attrib["title"]
             for feed in folder:
                 feed_url = feed.attrib["xmlUrl"]
-                requests.post(server + "/reader/api/0/subscription/quickadd?quickadd=" + feed_url, data={})
-                requests.post(server + "/reader/api/0/subscription/edit", data={"ac": "edit","s": "feed/" + feed_url,"a": ["user/-/label/" + title]})
+                requests.post(server + "/reader/api/0/subscription/quickadd?quickadd=" + feed_url, data={}, headers = headers)
+                requests.post(server + "/reader/api/0/subscription/edit", data={"ac": "edit","s": "feed/" + feed_url,"a": ["user/-/label/" + title]}, headers = headers)
