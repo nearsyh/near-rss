16add5dc0a8fc92370891500cb6e910837a19a56
Near <nearsy.h@gmail.com>
2021-05-22 19:47:17 +0800

    Support adding subscription

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -5,46 +5,47 @@ pub mod token;
 use crate::services::stream::StreamService;
 use crate::services::subscriptions::SubscriptionService;
 use crate::services::users::UserService;
+use reqwest;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct PageOption<T> {
-    pub offset: Option<T>,
-    pub limit: usize,
-    pub desc: bool,
+  pub offset: Option<T>,
+  pub limit: usize,
+  pub desc: bool,
 }
 
 impl<T> PageOption<T> {
-    pub fn new(limit: usize, desc: bool) -> Self {
-        Self {
-            offset: None,
-            limit: limit,
-            desc: desc,
-        }
+  pub fn new(limit: usize, desc: bool) -> Self {
+    Self {
+      offset: None,
+      limit: limit,
+      desc: desc,
     }
+  }
 }
 
 pub struct Page<T, OT> {
-    pub items: Vec<T>,
-    pub next_page_offset: Option<OT>,
+  pub items: Vec<T>,
+  pub next_page_offset: Option<OT>,
 }
 
 impl<T, OT> Page<T, OT> {
-    pub fn empty() -> Self {
-        Self {
-            items: vec![],
-            next_page_offset: None,
-        }
+  pub fn empty() -> Self {
+    Self {
+      items: vec![],
+      next_page_offset: None,
     }
+  }
 
-    pub fn convert<R, F>(self, f: F) -> Page<R, OT>
-    where
-        F: FnMut(T) -> R,
-    {
-        Page::<R, OT> {
-            items: self.items.into_iter().map(f).collect::<Vec<R>>(),
-            next_page_offset: self.next_page_offset,
-        }
+  pub fn convert<R, F>(self, f: F) -> Page<R, OT>
+  where
+    F: FnMut(T) -> R,
+  {
+    Page::<R, OT> {
+      items: self.items.into_iter().map(f).collect::<Vec<R>>(),
+      next_page_offset: self.next_page_offset,
     }
+  }
 }
 
 use rand::distributions::Alphanumeric;
@@ -52,29 +53,37 @@ use rand::Rng;
 use std::iter;
 
 pub fn new_id(length: usize) -> String {
-    iter::repeat(())
-        .map(|()| rand::thread_rng().sample(Alphanumeric))
-        .map(char::from)
-        .take(length)
-        .collect()
+  iter::repeat(())
+    .map(|()| rand::thread_rng().sample(Alphanumeric))
+    .map(char::from)
+    .take(length)
+    .collect()
 }
 
 pub fn current_time_ms() -> i64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis() as i64
+  SystemTime::now()
+    .duration_since(UNIX_EPOCH)
+    .unwrap()
+    .as_millis() as i64
 }
 
 pub fn current_time_s() -> i64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as i64
+  SystemTime::now()
+    .duration_since(UNIX_EPOCH)
+    .unwrap()
+    .as_secs() as i64
+}
+
+pub fn refresh() {
+  let endpoint = format!("{}/refresh", std::env::var("ENDPOINT").unwrap());
+  println!("Refreshing {}", endpoint);
+  if let Err(err) = reqwest::blocking::get(&endpoint) {
+    println!("Refreshing fails {:?}", err);
+  }
 }
 
 pub struct Services {
-    pub user_service: Box<dyn UserService + Send + Sync>,
-    pub subscription_service: Box<dyn SubscriptionService + Send + Sync>,
-    pub stream_service: Box<dyn StreamService + Send + Sync>,
+  pub user_service: Box<dyn UserService + Send + Sync>,
+  pub subscription_service: Box<dyn SubscriptionService + Send + Sync>,
+  pub stream_service: Box<dyn StreamService + Send + Sync>,
 }
--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -7,63 +7,108 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Contents {
-    items: Vec<ItemContent>,
-    next_page_offset: Option<String>,
+  items: Vec<ItemContent>,
+  next_page_offset: Option<String>,
 }
 
 #[get("/unread?<offset>&<limit>")]
 pub async fn get_unread_items(
-    auth_user: AuthUser,
-    services: &Services,
-    offset: Option<String>,
-    limit: Option<usize>,
+  auth_user: AuthUser,
+  services: &Services,
+  offset: Option<String>,
+  limit: Option<usize>,
 ) -> Json<Contents> {
-    let user_id = &auth_user.user.id;
-    let contents = services
-        .stream_service
-        .get_unread_item_contents(
-            user_id,
-            PageOption {
-                offset: offset,
-                limit: limit.unwrap_or(100),
-                desc: true,
-            },
-        )
-        .await
-        .unwrap();
-    Json(Contents {
-        items: contents.items,
-        next_page_offset: contents.next_page_offset,
-    })
+  let user_id = &auth_user.user.id;
+  let contents = services
+    .stream_service
+    .get_unread_item_contents(
+      user_id,
+      PageOption {
+        offset: offset,
+        limit: limit.unwrap_or(100),
+        desc: true,
+      },
+    )
+    .await
+    .unwrap();
+  Json(Contents {
+    items: contents.items,
+    next_page_offset: contents.next_page_offset,
+  })
 }
 
 #[options("/unread")]
 pub async fn get_unread_items_options() -> &'static str {
-    ""
+  ""
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Ids {
-    ids: Vec<String>,
+  ids: Vec<String>,
 }
 
 #[post("/markAsRead", data = "<ids>")]
 pub async fn mark_as_read(
-    auth_user: AuthUser,
-    services: &Services,
-    ids: Json<Ids>,
+  auth_user: AuthUser,
+  services: &Services,
+  ids: Json<Ids>,
 ) -> &'static str {
-    let str_ids = ids.ids.iter().map(|s| &**s).collect();
-    services
-        .stream_service
-        .mark_as_read(&auth_user.user.id, &str_ids)
-        .await
-        .unwrap();
-    "OK"
+  let str_ids = ids.ids.iter().map(|s| &**s).collect();
+  services
+    .stream_service
+    .mark_as_read(&auth_user.user.id, &str_ids)
+    .await
+    .unwrap();
+  "OK"
 }
 
 #[options("/markAsRead")]
 pub async fn mark_as_read_options() -> &'static str {
-    ""
+  ""
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SubscriptionData {
+  link: String,
+  title: Option<String>,
+  folder: Option<String>,
+}
+
+#[post("/addSubscription", data = "<subscription>")]
+pub async fn add_subscription(
+  auth_user: AuthUser,
+  services: &Services,
+  subscription: Json<SubscriptionData>,
+) -> &'static str {
+  let added = services
+    .subscription_service
+    .add_subscription_from_url(&auth_user.user.id, &subscription.link)
+    .await
+    .unwrap();
+  if let Some(ref f) = subscription.folder {
+    let tag = format!("user/-/label/{}", f);
+    let title: Option<&str> = if subscription.title.is_some() {
+      Some(&**subscription.title.as_ref().unwrap())
+    } else {
+      None
+    };
+    services
+      .subscription_service
+      .edit_subscription(&auth_user.user.id, &added.id, &title, &vec![&tag], &vec![])
+      .await
+      .unwrap();
+  }
+  services
+    .subscription_service
+    .load_all_subscription_items()
+    .await
+    .unwrap();
+  "OK"
+}
+
+#[options("/addSubscription")]
+pub async fn add_subscription_options() -> &'static str {
+  ""
 }
