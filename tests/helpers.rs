bf730be77993e1297ddd899fc95e528bdd16874c
Nearsyh <nearsy.h@gmail.com>
2022-11-12 12:15:04 +0800

    Add helper function to start server for tests

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -1,4 +1,15 @@
+use near_rss::create;
+
 pub struct TestApp {
     pub port: u16,
     pub address: String
+}
+
+pub async fn spawn_app() -> TestApp {
+    let rocket = create().await;
+    let _ = tokio::spawn(rocket.launch());
+    TestApp {
+        port: 8000,
+        address: "http://127.0.0.1:8000".into()
+    }
 }
